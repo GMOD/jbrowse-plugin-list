20717dbcfdc8979af11678b57e3ad34045f8bddb
Colin <colin.diesh@gmail.com>
2026-06-19 15:28:28 -0400

    Updates

---
+++ dist/@cmdcolin/jbrowse-plugin-hubs/1.0.9/dist/util.d.ts
@@ -0,0 +1,2 @@
+export declare function textfetch(url: string, arg?: RequestInit): Promise<string>;
+export declare function toDesktopSnapshot(jbrowse: Record<string, unknown>, session: Record<string, unknown>): Record<string, unknown>;
