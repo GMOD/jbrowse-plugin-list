092997e2dc77b6921dd580bdaf4705557c9baa4b
Colin <colin.diesh@gmail.com>
2026-08-17 13:48:41 -0400

    Updates: msaview 2.10.1, protein3d 0.8.3

--- dist/jbrowse-plugin-msaview/2.10.0/dist/LaunchMsaView/components/BlastQuery/useCachedBlastResults.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function useCachedBlastResults(geneIds: string[]): {
-    results: import("../../../utils/blastCache").CachedBlastResult[];
-    error: any;
-    isLoading: boolean;
-    handleDelete: (id: string) => Promise<void>;
-    handleClearAll: () => Promise<void>;
-};
